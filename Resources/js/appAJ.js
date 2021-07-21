21e38b0a029662d0906ff5ff08a0bc3da772222c
AJ P <10paineaj@gmail.com>
2021-07-20 20:16:36 -0600

    Fix units on radar plot

--- Resources/js/appAJ.js
+++ Resources/js/appAJ.js
@@ -354,14 +354,14 @@ d3.csv(csv, function(response){
   let matchedCity = response.filter(row => row.City == 'Denver');
   let match = matchedCity[0];
   //Set variables to plot
-  let matchCO = Math.round(((match.CO/1000)/15.4)*100)
+  let matchCO = Math.round((((match.CO/1.145)/1000)/9.5)*100)
   //let matchNH3 = match.NH3
   //let matchNO = match.NO
-  let matchNO2 = Math.round((match.NO2/649)*100)
-  let matchO3 = Math.round((match.O3/204)*100)
-  let matchSO2 = Math.round((match.SO2/304)*100)
-  let matchPM25 = Math.round((match.PM25/150.4)*100)
-  let matchPM10 = Math.round((match.PM10/354)*100)
+  let matchNO2 = Math.round(((match.NO2/1.88)/101)*100)
+  let matchO3 = Math.round(((match.O3/2)/125)*100)
+  let matchSO2 = Math.round(((match.SO2/2.62)/76)*100)
+  let matchPM25 = Math.round((match.PM25/35.5)*100)
+  let matchPM10 = Math.round((match.PM10/155)*100)
   let matchData = []
   matchData.push(matchCO, matchNO2, matchO3, matchSO2, matchPM25, matchPM10)
   radChart = new Chart(radar, {
@@ -430,14 +430,14 @@ d3.csv(csv, function(response){
     //Enter array of selected city
     let match = matchedCity[0];
     //Set variables to plot
-    let matchCO = Math.round(((match.CO/1000)/15.4)*100)
+    let matchCO = Math.round((((match.CO/1.145)/1000)/9.5)*100)
     //let matchNH3 = match.NH3
     //let matchNO = match.NO
-    let matchNO2 = Math.round((match.NO2/649)*100)
-    let matchO3 = Math.round((match.O3/204)*100)
-    let matchSO2 = Math.round((match.SO2/304)*100)
-    let matchPM25 = Math.round((match.PM25/150.4)*100)
-    let matchPM10 = Math.round((match.PM10/354)*100)
+    let matchNO2 = Math.round(((match.NO2/1.88)/101)*100)
+    let matchO3 = Math.round(((match.O3/2)/125)*100)
+    let matchSO2 = Math.round(((match.SO2/2.62)/76)*100)
+    let matchPM25 = Math.round((match.PM25/35.5)*100)
+    let matchPM10 = Math.round((match.PM10/155)*100)
     let matchData = []
     matchData.push(matchCO, matchNO2, matchO3, matchSO2, matchPM25, matchPM10)
     radChart = new Chart(radar, {
