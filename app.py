08c3efcfa76853f10a0036e0f1f078d3472c7e4d
William Serene <willserene@gmail.com>
2021-07-21 17:18:51 -0600

    minor sql edits

--- app.py
+++ app.py
@@ -1,186 +1,253 @@
 import numpy as np
+import pandas as pd
+import datetime as dt
 
-import sqlalchemy
+# import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 
-from flask import Flask, jsonify
+# from sqlalchemy import Metadata, Table
 
-import datetime as dt
+from flask import Flask, jsonify, render_template
+# from flask_cors import CORS
 
 
 #################################################
 # Database Setup
 #################################################
 
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-
+engine = create_engine("sqlite:///Resources/air.sqlite")
+# conn = engine.connect()
 # reflect database into a new model
 base = automap_base()
 
 # reflect the tables
 base.prepare(engine, reflect=True)
 
+
+
 # Save reference to the tables
-Measurement = base.classes.measurement
-Station = base.classes.station
+airquality = base.classes.airquality
+
+# inspector=inspect(engine)
+# inspector.get_table_names()
+
+# airquality = inspect.__name__
+
+# # Create a MetaData instance
+# metadata = MetaData(engine).reflect()
+# # print(metadata.tables)
+
+# # reflect db schema to MetaData
+# # metadata.reflect(bind=engine)
+# airquality = metadata.tables.airquality
+# print(airquality)
 
 #################################################
 # Flask Setup
 #################################################
 
 app = Flask(__name__)
+# CORS(app)
 
 #################################################
 # Flask Routes
 #################################################
 
+
+# @app.route("/")
+# def outside():
+# 	return render_template('index.html', title="page")
+
+
+
+
+
+
+#################################################
+# HW examples
+#################################################
+
+
 @app.route("/")
 def home():
 
-    url = 'http://127.0.0.1:5000'
+        session = Session(engine)
 
-    print("server received a request for 'Home' page")
-    
-    routes = (
-        f"<b>API Routes:</b> <p>"
-        f"<a href = " + url + "/api/v1.0/precipitation target = '_blank'>" + "/api/v1.0/precipitation" + "</a> <p>"
-        f"<a href = " + url + "/api/v1.0/stations target = '_blank'>" + "/api/v1.0/stations" + "</a> <p>"
-        f"<a href = " + url + "/api/v1.0/tobs target = '_blank'>" + "/api/v1.0/tobs" + "</a> <p>"
-        f"<a href = " + url + "/api/v1.0/YYYY-MM-DD target = '_blank'>" + "/api/v1.0/<startdate>YYYY-MM-DD" + "</a> <p>"
-        f"<a href = " + url + "/api/v1.0/YYYY-MM-DD/YYYY-MM-DD target = '_blank'>" + "/api/v1.0/<startdate>/<enddate>YYYY-MM-DD/YYYY-MM-DD" + "</a> <p>"
-    )
-    return routes
-    
-    #     f"Station Data: /api/v1.0/stations<br/>"
-    #     f"Temperature Data: /api/v1.0/tobs<br/>"
-    #     f"<br/>"
-    #     f"Min, Avg, and Max Temp Data from Start Date: /api/v1.0/<startdate><br/>"
-    #     f"Min, Avg, and Max Temp Data from Start Date to End Date: /api/v1.0/<startdate>/<enddate><br/>"
-    #     f"<br/>"
-    #     f"  *Input Start Date and End Date as YYYY-MM-DD<br/>"
-    #     f"    E.g. /api/v1.0/2016-04-15/2016-04-25"
-    # )
+        results = session.query(airquality)
 
-@app.route("/api/v1.0/precipitation")
-def precip():
-    session = Session(engine)
-    
-    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    
-    precip_data = session.query(func.strftime("%Y-%m-%d", Measurement.date), Measurement.prcp).\
-        filter(func.strftime("%Y-%m-%d", Measurement.date) >= query_date).all()
-
-    session.close()
-
-    precipitation_list = []
-    
-    for date, prcp in precip_data:
-        precipitation = {}
-        precipitation["date"] = date
-        precipitation["precipitation"] = prcp
-        precipitation_list.append(precipitation)
+        allcities = list(np.ravel(results))
         
-    return jsonify(precipitation_list)
+        session.close()
 
-# Return a JSON list of stations from the dataset.
+        return jsonify(allcities)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # airdata = session.query(func.max(airquality.AQI), func.min(airquality.AQI), func.avg(airquality.AQI))
+        
+        # airdata_results = list(np.ravel(airdata))
 
-@app.route("/api/v1.0/stations")
-def stations():
-    session = Session(engine)
+        # max = airdata_results.AQI[0]
+        # min = airdata_results.AQI[1]
+        # avg = airdata_results.AQI[2]
+
+        # session.close()
+
+        # airdata_list = []
     
-    stationresults = session.query(Station.station, Station.name).all()
+        # airdata_return = [
+        #     {"The minimum AQI on record": min},
+        #     {"The average AQI on record": avg},
+        #     {"The maximum AQI on record": max}]
+        # airdata_list.append(airdata_return)
+
+        # return jsonify(airdata_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    # url = 'http://127.0.0.1:5000'
+
+#     print("server received a request for 'Home' page")
+    
+#     routes = (
+#         f"<b>API Routes:</b> <p>"
+#         f"<a href = " + url + "/api/v1.0/precipitation target = '_blank'>" + "/api/v1.0/precipitation" + "</a> <p>"
+#         f"<a href = " + url + "/api/v1.0/stations target = '_blank'>" + "/api/v1.0/stations" + "</a> <p>"
+#         f"<a href = " + url + "/api/v1.0/tobs target = '_blank'>" + "/api/v1.0/tobs" + "</a> <p>"
+#         f"<a href = " + url + "/api/v1.0/YYYY-MM-DD target = '_blank'>" + "/api/v1.0/<startdate>YYYY-MM-DD" + "</a> <p>"
+#         f"<a href = " + url + "/api/v1.0/YYYY-MM-DD/YYYY-MM-DD target = '_blank'>" + "/api/v1.0/<startdate>/<enddate>YYYY-MM-DD/YYYY-MM-DD" + "</a> <p>"
+#     )
+#     return routes
+    
+    
+
+# @app.route("/api/v1.0/precipitation")
+# def precip():
+#     session = Session(engine)
+    
+#     query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    
+#     precip_data = session.query(func.strftime("%Y-%m-%d", Measurement.date), Measurement.prcp).\
+#         filter(func.strftime("%Y-%m-%d", Measurement.date) >= query_date).all()
+
+#     session.close()
+
+#     precipitation_list = []
+    
+#     for date, prcp in precip_data:
+#         precipitation = {}
+#         precipitation["date"] = date
+#         precipitation["precipitation"] = prcp
+#         precipitation_list.append(precipitation)
+        
+#     return jsonify(precipitation_list)
+
+# # Return a JSON list of stations from the dataset.
+
+# @app.route("/api/v1.0/stations")
+# def stations():
+#     session = Session(engine)
+    
+#     stationresults = session.query(Station.station, Station.name).all()
    
-    allstations = list(np.ravel(stationresults))
+#     allstations = list(np.ravel(stationresults))
     
-    session.close()
+#     session.close()
 
-    return jsonify(allstations)
+#     return jsonify(allstations)
 
 # Query the dates and temperature observations of the most active station for the last year of data.
 
 # Return a JSON list of temperature observations (TOBS) for the previous year.
 
-@app.route("/api/v1.0/tobs")
-def tobs():
-    session = Session(engine)
+# @app.route("/api/v1.0/tobs")
+# def tobs():
+#     session = Session(engine)
     
-    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    active_station = "USC00519281"
+#     query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+#     active_station = "USC00519281"
     
-    station_data = session.query(Measurement.date, Measurement.tobs).\
-        filter(Measurement.date >= query_date).\
-        filter(Measurement.station == active_station).all()
+#     station_data = session.query(Measurement.date, Measurement.tobs).\
+#         filter(Measurement.date >= query_date).\
+#         filter(Measurement.station == active_station).all()
 
-    most_active = list(np.ravel(station_data))
+#     most_active = list(np.ravel(station_data))
 
-    session.close()
+#     session.close()
 
-    return jsonify(most_active)
+#     return jsonify(most_active)
 
-# Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+# # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 
-@app.route("/api/v1.0/<startdate>")
-# route example: /api/v1.0/YYYY-MM-DD
-def tempdata_start(startdate = None):
+# @app.route("/api/v1.0/<startdate>")
+# # route example: /api/v1.0/YYYY-MM-DD
+# def tempdata_start(startdate = None):
 
-    session = Session(engine)
+#     session = Session(engine)
 
-    tempdata = session.query(func.max(Measurement.tobs), func.min(Measurement.tobs), func.avg(Measurement.tobs)).\
-        filter(Measurement.date >= startdate).all()
+#     tempdata = session.query(func.max(Measurement.tobs), func.min(Measurement.tobs), func.avg(Measurement.tobs)).\
+#         filter(Measurement.date >= startdate).all()
    
-    tempdata_results = list(np.ravel(tempdata))
+#     tempdata_results = list(np.ravel(tempdata))
 
-    tmax = tempdata_results[0]
-    tmin = tempdata_results[1]
-    tavg = tempdata_results[2].round(1)
+#     tmax = tempdata_results[0]
+#     tmin = tempdata_results[1]
+#     tavg = tempdata_results[2].round(1)
 
-    session.close()
+#     session.close()
 
-    tempdata_list = []
+#     tempdata_list = []
  
-    tempdata_return = [{"Start Date": startdate},
-        {"The minimum temperature on record from this day forward was": tmin},
-        {"The average temperature on record from this day forward was": tavg},
-        {"The maximum temperature on record from this day forward was": tmax}]
-    tempdata_list.append(tempdata_return)
+#     tempdata_return = [{"Start Date": startdate},
+#         {"The minimum temperature on record from this day forward was": tmin},
+#         {"The average temperature on record from this day forward was": tavg},
+#         {"The maximum temperature on record from this day forward was": tmax}]
+#     tempdata_list.append(tempdata_return)
 
-    return jsonify(tempdata_list)
+#     return jsonify(tempdata_list)
     
-#When given the start & the end date, calculate the 'TMIN', 'TAVG', & 'TMAX' for dates between the start & end date inclusive.
+# #When given the start & the end date, calculate the 'TMIN', 'TAVG', & 'TMAX' for dates between the start & end date inclusive.
 
-@app.route("/api/v1.0/<startdate>/<enddate>")
-# route example: /api/v1.0/YYYY-MM-DD/YYY-MM-DD
-def tempdata_daterange(startdate = None, enddate = None):
+# @app.route("/api/v1.0/<startdate>/<enddate>")
+# # route example: /api/v1.0/YYYY-MM-DD/YYY-MM-DD
+# def tempdata_daterange(startdate = None, enddate = None):
 
-    session = Session(engine)
+#     session = Session(engine)
     
-    tempdata = session.query(func.max(Measurement.tobs), func.min(Measurement.tobs), func.avg(Measurement.tobs)).\
-        filter(Measurement.date >= startdate).filter(Measurement.date <= enddate).all()
+#     tempdata = session.query(func.max(Measurement.tobs), func.min(Measurement.tobs), func.avg(Measurement.tobs)).\
+#         filter(Measurement.date >= startdate).filter(Measurement.date <= enddate).all()
 
-    tempdata_results = list(np.ravel(tempdata))
+#     tempdata_results = list(np.ravel(tempdata))
 
-    tmax = tempdata_results[0]
-    tmin = tempdata_results[1]
-    tavg = tempdata_results[2].round(1)
+#     tmax = tempdata_results[0]
+#     tmin = tempdata_results[1]
+#     tavg = tempdata_results[2].round(1)
 
-    tempdata_results = []
+#     tempdata_results = []
     
-    tempdata_return = [{"Start Date": startdate}, {"End Date": enddate},
-        {"The minimum temperature during this period of time was": tmin},
-        {"The average temperature during this period of time was": tavg},
-        {"The maximum temperature during this period of time was": tmax}]
+#     tempdata_return = [{"Start Date": startdate}, {"End Date": enddate},
+#         {"The minimum temperature during this period of time was": tmin},
+#         {"The average temperature during this period of time was": tavg},
+#         {"The maximum temperature during this period of time was": tmax}]
         
-    tempdata_results.append(tempdata_return)
+#     tempdata_results.append(tempdata_return)
 
-    session.close()
+#     session.close()
 
-    return jsonify(tempdata_results)
+#     return jsonify(tempdata_results)
 
        
     
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 
